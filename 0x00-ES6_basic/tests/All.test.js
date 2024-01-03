8096d56d8dd0b53b2f1987385856fcf6d5942219
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-01-03 03:38:28 +0200

    finish task 4 and add Unittests to it and lint the code

--- 0x00-ES6_basic/tests/All.test.js
+++ 0x00-ES6_basic/tests/All.test.js
@@ -2,6 +2,10 @@
 import { taskFirst, getLast, taskNext } from "../0-constants";
 import taskBlock  from "../1-block-scoped";
 import getNeighborhoodsList  from "../2-arrow";
+import fs from 'fs';
+import path from 'path';
+import getSumOfHoods from '../3-default-parameter.js';
+
 describe('0-constants.js', () => {
   // Test Case 1
   test('A Test for taskFirst Function', () => {
@@ -38,5 +42,25 @@ describe('2-arrow_functions.js', () => {
     expect(res).toEqual([ 'SOMA', 'Union Square', 'Noe Valley' ]);
     const resTwo = neighborhoodsList.addNeighborhood('new neighborhood');
     expect(resTwo).toContain('new neighborhood');
+  });
+});
+
+// Path: 0x00-ES6_basic/tests/3-default_parameter.test.js
+describe('3-default_parameter.js', () => {
+  // Test Case 1
+  test('Test the getSumOfHoods function', () => {
+    expect(getSumOfHoods(20, 50, 20)).toEqual(90);
+    expect(getSumOfHoods(20, 50)).toEqual(89);
+    expect(getSumOfHoods(20)).toEqual(128);
+  });
+  // Test Case 2
+  test('Test the getSumOfHoods function with no arguments', () => {
+    expect(getSumOfHoods()).toBe(NaN);
+  });
+  // Test Case 3
+  test('Test the length of the file', () => {
+    const data = fs.readFileSync(path.resolve(__dirname, '../3-default-parameter.js'), 'utf8');
+    const lines = data.split('\n').length;
+    expect(lines).toEqual(4);
   });
 });
