902930d0e19ee1b224920a5b18ffa789014b1221
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-01-03 06:02:41 +0200

    fix task 11 change for in loop to for of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const Arr = array;
   for (const idx of Arr) {
-    Arr[Arr.indexOf(idx)] = appendString + idx;
+    Arr[array.indexOf(idx)] = appendString + idx;
   }
 
   return Arr;
