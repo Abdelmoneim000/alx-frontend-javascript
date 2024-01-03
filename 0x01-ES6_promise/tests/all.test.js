cee920d0a5491a0f2c40379d26f15dca6b086714
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-01-03 10:43:05 +0200

    create unit tests and finish task 1 promise response

--- 0x01-ES6_promise/tests/all.test.js
+++ 0x01-ES6_promise/tests/all.test.js
@@ -1,11 +1,30 @@
 // START TEST SETUP
 // Import functions to be tested
-import getResponseFromAPI from "../0-promise";
-
+import getResponseFromAPI from "../1-promise";
 
 describe("0-promise.js", () => {
     // Test Case 1
     test("should return a promise", () => {
-        expect(getResponseFromAPI()).toBeInstanceOf(Promise);
+        expect(getResponseFromAPI(true)).toBeInstanceOf(Promise);
+    });
+});
+
+describe("1-promise.js", () => {
+    // Test Case 1
+    test("should return a promise", () => {
+        expect(getResponseFromAPI(true)).toBeInstanceOf(Promise);
+    });
+    // Test Case 2
+    test("should return correct response when passed true", () => {
+        return getResponseFromAPI(true).then((response) => {
+            expect(response.status).toEqual(200);
+            expect(response.body).toEqual("Success");
+        });
+    });
+    // Test Case 3
+    test("should return correct error when passed false", () => {
+        return getResponseFromAPI(false).catch((error) => {
+            expect(error).toEqual(Error("The fake API is not working currently"));
+        });
     });
 });
