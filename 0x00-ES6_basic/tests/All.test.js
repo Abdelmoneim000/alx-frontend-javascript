59fb07db2bb7adb19999219cb2a9a334a6216fa7
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-01-03 03:20:26 +0200

    Finish task 2 arrow function and add unit tests to it

--- 0x00-ES6_basic/tests/All.test.js
+++ 0x00-ES6_basic/tests/All.test.js
@@ -1,6 +1,7 @@
 // BEGIN: Test Suite
 import { taskFirst, getLast, taskNext } from "../0-constants";
 import taskBlock  from "../1-block-scoped";
+import getNeighborhoodsList  from "../2-arrow";
 describe('0-constants.js', () => {
   // Test Case 1
   test('A Test for taskFirst Function', () => {
@@ -25,5 +26,17 @@ describe('1-block_scope.js', () => {
   test('Test the taskBlock function', () => {
     expect(taskBlock(true)).toEqual([false, true]);
     expect(taskBlock(false)).toEqual([false, true]);
+  });
+});
+
+// Path: 0x00-ES6_basic/tests/2-arrow_functions.test.js
+describe('2-arrow_functions.js', () => {
+  // Test Case 1
+  test('Test the getNeighborhoodsList function', () => {
+    const neighborhoodsList = new getNeighborhoodsList();
+    const res = neighborhoodsList.addNeighborhood('Noe Valley');
+    expect(res).toEqual([ 'SOMA', 'Union Square', 'Noe Valley' ]);
+    const resTwo = neighborhoodsList.addNeighborhood('new neighborhood');
+    expect(resTwo).toContain('new neighborhood');
   });
 });
