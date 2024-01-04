633130118b7c31d9e79835f748af05b2b34e9f89
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-01-04 23:08:37 +0200

    finish task 10 implement a function to create 3 classes of ClassRoom and add its tests

--- 0x02-ES6_classes/tests/All.test.js
+++ 0x02-ES6_classes/tests/All.test.js
@@ -1,17 +1,28 @@
 // START TESTS
 // Import Classes
 import ClassRoom from "../0-classroom";
+import initializeRooms from "../1-make_classrooms";
 
 describe("ClassRoom", () => {
   it("should create a ClassRoom instance with a maxStudentSize", () => {
     const classRoom = new ClassRoom(10);
-    expect(classRoom).toEqual({ _maxStudentSize: 10 });
+    expect(classRoom).toEqual({ _maxStudentsSize: 10 });
   });
   it("should create a ClassRoom instance with undefined _maxStudentSize", () => {
     const classRoom = new ClassRoom();
-    expect(classRoom).toEqual({ _maxStudentSize: undefined });
+    expect(classRoom).toEqual({ _maxStudentsSize: undefined });
   });
 });
 
+describe("initializeRooms", () => {
+    it("should return an array of ClassRoom instances", () => {
+        const classRooms = initializeRooms();
+        expect(classRooms).toEqual([
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34)
+        ]);
+    });
+});
 
 // END TESTS
