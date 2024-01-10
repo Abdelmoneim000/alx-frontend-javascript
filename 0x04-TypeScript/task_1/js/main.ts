e12c97e0a239fb98d5c6d65a234f1339d226c4c9
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-01-10 09:55:41 +0200

    finish task 5 creating the studentClass class and add its interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,33 @@ function printTeacher(firstName : string, lastName : string) : string {
     
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+// To ensure the constructor adheres to the interface
+const Student: StudentConstructor = StudentClass;
